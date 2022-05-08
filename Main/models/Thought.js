07829c365cc03fd5134396d1a858baf9000e9b1f
Quadrilateral <amandacdulaney@gmail.com>
2022-05-08 11:08:15 -0400

    built out thoughtSchema

---
+++ Main/models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    thoughtText: {
+      type: String,
+      required: true,
+      max_length: 280,
+      min_length: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
